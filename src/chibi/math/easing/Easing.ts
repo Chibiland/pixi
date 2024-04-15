8f2e6b7a00a237b5609a07ecc4e1ef1974c2cd78
Feavy <quentin071100@live.fr>
2024-04-15 18:09:52 +0200

    Init easing functions

---
+++ src/chibi/math/easing/Easing.ts
@@ -0,0 +1,12 @@
+import Linear from "./Linear";
+
+/**
+ * Usual easing functions.
+ * To customize their parameters, create new instances.
+ */
+namespace Easing {
+    // Linear
+    export const linear = Linear.INSTANCE;
+}
+
+export default Easing;
---
+++ src/chibi/math/easing/EasingFunction.ts
@@ -0,0 +1,7 @@
+export default abstract class EasingFunction {
+    abstract apply(t: number): number;
+
+    public interpolate(start: number, end: number, t: number): number {
+        return start + (end - start) * this.apply(t);
+    }
+}
